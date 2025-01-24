f3c3db4707594f56a78493604757a6c49c7a234c
Judith <jbarrilnavarro@gmail.com>
2025-01-24 19:29:47 +0100

    assets a public

--- src/import.js
+++ src/import.js
@@ -1,14 +1,14 @@
-const logoGithub = './src/assets/github-logo.png';
-const logoInstagram = './src/assets/instagram.png';
-const logoLinkedin = './src/assets/linkedin.png';
+const logoGithub = '/github-logo.png';
+const logoInstagram = '/instagram.png';
+const logoLinkedin = '/linkedin.png';
 const judithContact = '/judith-barril-navarro.jpg';
-const logo = 'src/assets/logo.png';
-const profile = 'src/assets/profile.png';
-const siSoy = 'src/assets/sisoyJB.webp';
-const scroll = './src/assets/scroll.svg';
+const logo = '/logo.png';
+const profile = '/profile.png';
+const siSoy = '/sisoyJB.webp';
+const scroll = '/scroll.svg';
 
-const coverChatBot = './src/assets/chatbot-gemini.jpg';
-const coverPat = 'src/assets/webpat.jpg';
+const coverChatBot = '/chatbot-gemini.jpg';
+const coverPat = '/webpat.jpg';
 
 export {
   logoGithub,
